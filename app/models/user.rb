493d13f54dda9e58ae09f4d837b6dda2112f1274
Pat Heard <patrick.heard@cds-snc.ca>
2022-11-10 22:58:11 +0000

    chore: resource cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
